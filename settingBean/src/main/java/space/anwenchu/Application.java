77c3f96c681c4c142866d658190e6439a467cf29
an_wch <an_wch@suixingpay>
2017-08-28 17:59:53 +0800

    test

--- settingBean/src/main/java/space/anwenchu/Application.java
+++ settingBean/src/main/java/space/anwenchu/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Created by an_wch on 2017/8/28.
+ * Created by an_wch on 2017/8/28..
  */
 @SpringBootApplication
 public class Application {
