2384a4908565a4239221562ff4651af66b9865a5
an_wch <an_wch@suixingpay>
2017-11-28 16:30:18 +0800

    eureka

--- aop/src/main/java/space/anwenchu/config/WebMvcConfigurer.java
+++ aop/src/main/java/space/anwenchu/config/WebMvcConfigurer.java
@@ -3,7 +3,6 @@ package space.anwenchu.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import space.anwenchu.interceptor.MyInterceptor;
 
